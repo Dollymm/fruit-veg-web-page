6152addc8e994061b995ad594b7d359b7586875f
Dollymm <100014305+Dollymm@users.noreply.github.com>
2023-04-08 14:46:41 +0530

    fruit veg commit

---
+++ fruit veg website/script.js
@@ -0,0 +1,12 @@
+//swiper
+
+var swiper = new swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+  
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
